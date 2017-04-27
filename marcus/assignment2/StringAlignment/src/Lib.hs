77e4ff6c3f9d6e8b77990d45cbcdf0cbf3e0991a
Marcus Rodan <marcusrodan@gmail.com>
2017-04-27 14:28:23 +0200

    attachHeads comment

--- marcus/assignment2/StringAlignment/src/Lib.hs
+++ marcus/assignment2/StringAlignment/src/Lib.hs
@@ -24,3 +24,17 @@ similarityScore alls@(s:ss) allt@(t:ts) = maximum [keepLetters, spaceS, spaceT]
           keepScore   = if (s == t) then scoreMatch else scoreMismatch
           spaceS      = scoreSpace + similarityScore alls ts
           spaceT      = scoreSpace + similarityScore ss allt
+
+-- | Takes two elements and a list of pairs where each pair consists of two lists.
+--   The first element is prepended to the first list of all pairs. The second
+--   element is prepended to the second list of all pairs. The modified pairs
+--   are returned as a list.
+--
+--   Examples:
+--
+--   >>> attachHeads 1 2 [([], []), ([], [])]
+--   [([1],[2]),([1],[2])]
+--   >>> attachHeads 'a' 'b' [([],[]),("pple","ike")]
+--   [("a","b"),("apple","bike")]
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]

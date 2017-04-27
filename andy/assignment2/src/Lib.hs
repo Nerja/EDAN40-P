c0ff0d311290270411094859d19d0e66da427027
AndyT94 <andytruong94@gmail.com>
2017-04-27 21:58:58 +0200

    similarityScore

--- andy/assignment2/src/Lib.hs
+++ andy/assignment2/src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    (   scoreMatch
+      , scoreMismatch
+      , scoreSpace
+      , similarityScore
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+scoreMatch = 0
+scoreMismatch = -1
+scoreSpace = -1
+
+similarityScore :: String -> String -> Int
+similarityScore [] ys = (*) scoreSpace $ length ys
+similarityScore xs [] = (*) scoreSpace $ length xs
+similarityScore (x:xs) (y:ys) = maximum [(+) (similarityScore xs ys) $ score x y, (+) scoreSpace $ similarityScore xs (y:ys), (+) scoreSpace $ similarityScore (x:xs) ys]
+
+score :: Char -> Char -> Int
+score x y
+  | x == '-' || y == '-' = scoreSpace
+  | x == y               = scoreMatch
+  | otherwise            = scoreMismatch
--- andy/assignment2/test/Spec.hs
+++ andy/assignment2/test/Spec.hs
@@ -2,10 +2,18 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Lib
 
+similarityScoreTest :: TestTree
+similarityScoreTest = testGroup "Unit tests for similarityScore"
+  [
+      testCase "Empty string1" $ similarityScore "" "AAA" @?= -3
+    , testCase "Empty string2" $ similarityScore "AA" "" @?= -2
+    , testCase "writers and vintner" $ similarityScore "writers" "vintner" @?= -5
+  ]
+
 unitTests :: TestTree
 unitTests = testGroup "All unit tests"
   [
-
+      similarityScoreTest
   ]
 
 main = defaultMain unitTests

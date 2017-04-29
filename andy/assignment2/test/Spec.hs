25dde6e714fe9f7a3cb1e4145c207db5e9e08384
AndyT94 <andytruong94@gmail.com>
2017-04-29 13:22:25 +0200

    maximaBy

--- andy/assignment2/test/Spec.hs
+++ andy/assignment2/test/Spec.hs
@@ -10,10 +10,21 @@ similarityScoreTest = testGroup "Unit tests for similarityScore"
     , testCase "writers and vintner" $ similarityScore "writers" "vintner" @?= -5
   ]
 
+
+maximaByTest :: TestTree
+maximaByTest = testGroup "Unit tests for maximaByTest"
+  [
+      testCase "maximum length strings" $ maximaBy length ["cs", "efd", "lth", "it"] @?= ["efd","lth"]
+    , testCase "maximum length empty" $ maximaBy length [[1]] @?= [[1]]
+    , testCase "maximum length single" $ maximaBy length ["hehe", "trolllol", "hehe"] @?= ["trolllol"]
+    , testCase "maximum length all" $ maximaBy length ["grill", "kaffe", "nerja"] @?= ["grill", "kaffe", "nerja"]
+  ]
+
 unitTests :: TestTree
 unitTests = testGroup "All unit tests"
   [
       similarityScoreTest
+    , maximaByTest
   ]
 
 main = defaultMain unitTests
